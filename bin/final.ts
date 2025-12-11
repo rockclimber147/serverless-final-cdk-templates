2e5a3965d55d6e4c3df65b8293adc35f4571ce3c
rockclimber147 <daylencsmith@gmail.com>
2025-12-11 15:00:35 -0800

    rename stuff

--- bin/final.ts
+++ bin/final.ts
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { FinalStackPart1 } from '../lib/final-stack-part-1';
-import { FinalStackPart2 } from '../lib/final-stack-part-2'; // <-- NEW
+import { FinalStackPart1 as DaylenSmithStack1 } from '../lib/final-stack-part-1';
+import { FinalStackPart2 as DaylenSmithStack2 } from '../lib/final-stack-part-2'; // <-- NEW
 
 const app = new cdk.App();
 
 // Stack 1: Simple API Gateway -> Lambda (Messaging)
-new FinalStackPart1(app, 'FinalStackPart1', {
+new DaylenSmithStack1(app, 'DaylenSmithStack1', {
   // Use a fixed environment to ensure the deployment works smoothly
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '123456789012', region: 'us-west-1' },
 });
 
 // Stack 2: API Gateway -> Lambda -> DynamoDB (CRUD)
-new FinalStackPart2(app, 'FinalStackPart2', { // <-- NEW
+new DaylenSmithStack2(app, 'DaylenSmithStack2', { // <-- NEW
   // Use a fixed environment to ensure the deployment works smoothly
   // env: { account: '123456789012', region: 'us-east-1' },
 });
